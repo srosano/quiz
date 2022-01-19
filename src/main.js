41a1676f251423b3e2be94dc8cd949a5a7c22522
scott rosano <scott.rosano@gmail.com>
2022-01-19 16:54:15 +1100

    quiz done in my spare time https://www.youtube.com/watch?v=4deVCNJq3qc

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+/*Relative dependancies last */
 import App from './App.vue'
 
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
